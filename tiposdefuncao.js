8fbae10729bcaa9f46446b5e7c8ebfcc42374d1e
oliverbgeiger <oliverbgeiger@gmail.com>
2021-12-20 21:13:27 -0300

    calculadora prompt

---
+++ tiposdefuncao.js
@@ -0,0 +1,19 @@
+// tipo de funcao
+// declarativas
+//function funcao() {
+//console.log("mensagem para voce")
+//
+
+//expressão de função
+//com nomeacao
+
+//var funcao = function funcao () {
+//  console.log("tchau opara voces");
+//}
+
+//funcao();
+
+var funcao = () => {
+console.log("sou uma errow function");
+}
+funcao ()
